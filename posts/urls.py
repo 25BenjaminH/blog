b065bfe7e012d888d84a6babcd252da7413e8739
Ben <25BenjaminH@students.tas.tw>
2021-04-24 12:05:01 +0800

    kkk

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,6 @@ from . import views
 # 網頁路徑 網頁在 views 裡的功能 名稱
 urlpatterns = [
     path('COVID19_Map/', views.covid19_map, name = "covid19_map"),
+    path('data_storage/', views.data_storage, name = "data_storage"),
+    path('projects/',views.projects, name = 'projects')
 ]
--- posts/views.py
+++ posts/views.py
@@ -4,3 +4,9 @@ import requests
 # Create your views here.
 def covid19_map(request):
     return render(request,'posts/covid19_map.html')
+
+def data_storage(request):
+    return render(request,'posts/data_storage.html')
+
+def projects(request):
+    return render(request,'posts/projects.html')
