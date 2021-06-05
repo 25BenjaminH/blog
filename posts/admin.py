9575876821721b0fff5428956e58b0fad849e48c
Ben <25BenjaminH@students.tas.tw>
2021-06-05 11:40:21 +0800

    updated

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Country_total_deaths
+
+
+class Country_total_deaths_Admin(admin.ModelAdmin):
+    list_display = ('id','country','deaths')
+
+
 
 # Register your models here.
+admin.site.register(Country_total_deaths,Country_total_deaths_Admin)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Country_total_deaths(models.Model):
+    country = models.CharField(max_length=255)
+    deaths = models.IntegerField(max_length=255)
+    def __str__(self):
+        return self.country, self.deaths
+
+    
